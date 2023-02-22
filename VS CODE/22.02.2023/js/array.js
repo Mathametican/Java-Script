cb2a35058bc3d1249e3bedc06fcfabd57cdba8f5
Kübra Sert <elmastas571@gmail.com>
2023-02-23 02:29:35 +0300

    List in JavaScript

---
+++ VS CODE/22.02.2023/js/array.js
@@ -0,0 +1,14 @@
+var x=[23,34,"Ayşe"];
+//document.getElementById("demo1").innerHTML=x[3];
+document.getElementById("demo1").innerHTML=x.length;
+
+var y=[];
+y[0]=12;
+y[1]=33;
+y[2]=24;
+y[3]=15;
+document.getElementById("demo2").innerHTML=y.sort();
+
+var z=new Array(96,"Hacer");
+document.getElementById("demo3").innerHTML=z;
+

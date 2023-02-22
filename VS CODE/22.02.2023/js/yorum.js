345e572d1645ec1ffaed40b221c31b8967ca5e57
Kübra Sert <elmastas571@gmail.com>
2023-02-23 02:49:50 +0300

    new

---
+++ VS CODE/22.02.2023/js/yorum.js
@@ -0,0 +1,13 @@
+const y=[];
+
+function Ekle(){
+    var a=document.getElementById("yy").value 
+    y.push(a,"<br>");
+    document.getElementById("pp").innerHTML=y;
+}
+
+function Sil(){
+    //pop enson ekleneni siler
+    y.pop();
+    document.getElementById("pp").innerHTML=y;
+}

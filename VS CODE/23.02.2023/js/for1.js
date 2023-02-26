377bfc9b93c6fea4a5c021587f6e44830163f1c1
Kübra Sert <elmastas571@gmail.com>
2023-02-26 10:00:55 +0300

    new changes

---
+++ VS CODE/23.02.2023/js/for1.js
@@ -0,0 +1,4 @@
+for(var i=0; i<10; i++){
+   // document.write("Selamün Aleyküm"+"</br>");
+   document.write("<span style='font-size:"+i+5+"px;'> Selamün Aleyküm</span>"+"</br>");
+}
---
+++ VS CODE/23.02.2023/js/for2.js
@@ -0,0 +1,5 @@
+var mesaj=prompt("Enter yout message");
+var sayi=prompt("How many time the loop is run?");
+for(var i=0; i<=parseInt(sayi); i++){
+    document.write(mesaj+"</br>");
+}

5b83c8e4bca6dfdbf4d689271c51f6b60d26f6ce
Kübra Sert <elmastas571@gmail.com>
2023-02-27 19:15:28 +0300

    new changes

---
+++ VS CODE/27.02.2023/js/try3.js
@@ -0,0 +1,26 @@
+function testtrycatch(){
+    let x=document.getElementById("in1").value;
+    let y=document.getElementById("pid");
+    y.innerHTML="";
+    y.style.color="red";
+    try{
+    if(x==""){
+        throw "Input connot be Empty ";
+    }
+    if(x<5){
+        throw "Input cannot be less than 5";
+    }
+    if(x>10){
+        throw "Input cannot be greater than 10";
+    }
+    if(isNaN(x)){
+        throw "Input must be a number";
+    }
+    if(x>=5 && x<=10){
+        y.innerHTML="you did good job";
+        y.style.color="green";
+    }
+    }catch(e){
+    y.innerHTML=e;
+    }
+}

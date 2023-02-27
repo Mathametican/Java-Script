7198ce94456e1b59998fb376f41bdc56e29fbb90
Kübra Sert <elmastas571@gmail.com>
2023-02-27 12:08:25 +0300

    css in javascript

--- VS CODE/23.02.2023/js/for3.js
+++ VS CODE/23.02.2023/js/for3.js
@@ -1,7 +1,8 @@
-document.write("<table border='50'><tr>");
+document.write("<h1 style='font-size:25px; color:blue'>ÇARPIM TABLOSU: </h1>");
+document.write("<table border='10' style='font-size:20px; color:red; background:yellow' > <tr>");
 for(var i=1; i<=15; i++){
     document.write("<td>");
-    for(var x=1; x<=10;x++){
+    for(var x=1; x<=15;x++){
     document.write(i+'*'+x+'=' +i*x+'</br>');
     }
     document.write("</td>");

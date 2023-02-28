740d14382b4ee4e0cdaa13196bab56c2c5a6cfc3
Kübra Sert <elmastas571@gmail.com>
2023-02-28 19:04:48 +0300

    new changes

---
+++ VS CODE/28.02.2023/js/query.js
@@ -0,0 +1,7 @@
+function QueryTest(){
+var x=document.getElementsByClassName("xx");
+x[1].style.color="blue";
+x[0].style.fontSize="100px";
+x[2].innerHTML="Test başarılı";
+x[3].style.background="pink";
+}

973d32d633a42571b26fe860083c5746574b339f
Kübra Sert <elmastas571@gmail.com>
2023-03-06 00:56:07 +0300

    new changes

---
+++ VS CODE/03.03.2023/js/app1.js
@@ -0,0 +1,12 @@
+var x1="Merhaba";
+var x2=52;
+var x3=false;
+var x4=[];
+var x5=function MyFunction(){
+
+}
+var x6={};
+
+document.getElementById("type").innerHTML= typeof x1+"</br>"+
+typeof x2+"</br>"+typeof x3+"</br>"+typeof x4+"</br>"+
+typeof x5+"</br>"+typeof x6+"</br>";

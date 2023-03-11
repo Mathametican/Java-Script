238daa0efec8edc62a64b15b7520b1b964310c90
Kübra Sert <elmastas571@gmail.com>
2023-03-12 01:49:54 +0300

    new changes in javascript

--- VS CODE/07.03.2023/js/es-6app3.js
+++ VS CODE/07.03.2023/js/es-6app3.js
@@ -7,3 +7,10 @@ function ToplaES5(){
 function sum(a,b){
     return a+b;
 }
+
+function ToplaES6(){
+    var x=Number(document.getElementById("in1").value);
+    var y=Number(document.getElementById("in2").value);
+    var s=(x,y)=>{return x+y;};
+    document.getElementById("demo").innerHTML=s(x,y);
+}

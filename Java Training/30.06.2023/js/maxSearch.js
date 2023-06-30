d3b63dd1a2874db371b09c3f59348a48ef35fa66
Kübra Sert <elmastas571@gmail.com>
2023-06-30 23:21:00 +0300

    writing max and min finding code

--- Java Training/30.06.2023/js/maxSearch.js
+++ Java Training/30.06.2023/js/maxSearch.js
@@ -1,32 +1,38 @@
 //document.getElementById('sonuc').innerHTML=Math.min(9,6);
 //We create a function also it can find max and min value in a group of variable
 
-function MaxFind(){
-    var number1=document.getElementById('in1').value;
-    var number2=document.getElementById('in2').value;
 
-    if(number1>number2){
+
+function MaxFind(){
+    
+    var number1=Number(document.getElementById('in1').value);
+    var number2=Number(document.getElementById('in2').value);
+    var number3=Number(document.getElementById('in3').value);
+
+    if(number1>number2 && number1>number3){
         document.getElementById('sonuc1').innerHTML="The maximum value is the :"+number1;
     }
-    else if(number1==number2){
-        document.getElementById('sonuc1').innerHTML="The Numbers are equal."
+    else if(number2>number1 && number2>number3){
+        document.getElementById('sonuc1').innerHTML="The maximum value is the :"+number2;
     }
     else{
-    document.getElementById('sonuc1').innerHTML="The maximum value is the : "+number2;
+    document.getElementById('sonuc1').innerHTML="The maximum value is the : "+number3;
     }
 }
 
 function MinFind(){
-    var number1=document.getElementById('in1').value;
-    var number2=document.getElementById('in2').value;
 
-    if(number1<number2){
-        document.getElementById('sonuc1').innerHTML="The mimimum value is the :"+number1;
+    var number1=Number(document.getElementById('in1').value);
+    var number2=Number(document.getElementById('in2').value);
+    var number3=Number(document.getElementById('in3').value);
+
+    if(number1<number2 && number1<number3){
+        document.getElementById('sonuc1').innerHTML="The mimimum value is the : "+number1;
     }
-    else if(number1==number2){
-        document.getElementById('sonuc1').innerHTML="The Numbers are equal."
+    else if(number2<number1 && number2<number3){
+        document.getElementById('sonuc1').innerHTML="The mimimum value is the : "+number2;
     }
     else{
-    document.getElementById('sonuc1').innerHTML="The minimum value is the : "+number2;
+    document.getElementById('sonuc1').innerHTML="The minimum value is the : "+number3;
     }
 }

2e79ed0c8da1539c512e4c1e97373731cbe5595f
Kübra Sert <elmastas571@gmail.com>
2023-07-04 23:47:12 +0300

    Training lesson for july 3

---
+++ Java Training/04.07.23/js/for2.js
@@ -0,0 +1,7 @@
+var x=prompt("Please enter a number");  //Alert gibi ekran açılınca input ekranı getiriyor.
+
+console.log(x);
+
+for(var i=0;i<=Number(x);i++){
+    document.write(i+" Hello World"+"<br>");
+}

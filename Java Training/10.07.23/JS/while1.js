2e4a8d9bf8bf86d854628e7b5c3833e4753f0189
Kübra Sert <elmastas571@gmail.com>
2023-07-11 01:15:06 +0300

    New  working about java script

---
+++ Java Training/10.07.23/JS/while1.js
@@ -0,0 +1,11 @@
+var a=1;
+while(a<=10){
+    document.write("Merhaba"+"<br>")
+    a++; //a++ satırı sonsuz döndüye girmemesi için yapılıyor yoksa 1<=10 her zaman doğru olur.
+}
+
+for(var i=1; i<=10; i++){
+
+    document.write("Hello"+"<br>")
+}
+//dönme sayısı belli ise for belli değil ise while döngüsü KUllanılır.
